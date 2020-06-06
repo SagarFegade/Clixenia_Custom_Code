2f303fbd074eb2a171404817b16345755b833ed8
Sagar Fegade <sfsagar7@gmail.com>
2020-06-06 15:09:09 +0530

    Create Functions.php

---
+++ Functions.php
@@ -0,0 +1,7 @@
+// add this code to plugin or functions.php of divi
+//Shortcode to show the module
+function showmodule_shortcode($moduleid) {
+extract(shortcode_atts(array('id' =>'*'),$moduleid)); 
+return do_shortcode('[et_pb_section global_module="'.$id.'"][/et_pb_section]');
+}
+add_shortcode('showmodule', 'showmodule_shortcode');
